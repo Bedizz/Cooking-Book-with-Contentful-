911adc16d73961188b43db1f09e41b5fb0f2cd01
Bedizz <turkyilmaz.bediz@gmail.com>
2024-01-17 15:46:49 +0100

    now we have navlink

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,24 +1,26 @@
-import React from 'react'; 
-import '../App.css'; 
+import React from "react";
+import "../App.css";
+import { NavLink } from "react-router-dom";
 
 const NavBar = () => {
-    return (
-        <nav>
-  <ul>
-    <li>
-      <a href="">Home</a>
-    </li>
-    <li>
-      <img src="public\Recipe Images\cook-book-logo-3-removebg-preview.png" alt="cook-book" />
-    </li>
-    <li>
-      <a href="/recipes">Recipes</a>
-    </li>
-    
-  </ul>
-</nav>
-    )
+  return (
+    <nav>
+      <ul>
+        <li>
+          <NavLink path="/">HomePage</NavLink>
+        </li>
+        <li>
+          <img
+            src="Recipe Images\cook-book-logo-3-removebg-preview.png"
+            alt="cook-book"
+          />
+        </li>
+        <li>
+          <NavLink path="/recipes">Recipes</NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
 };
 
 export default NavBar;
-
