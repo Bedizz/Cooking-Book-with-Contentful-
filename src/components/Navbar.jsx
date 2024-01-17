25d4d2430724694cde0b3188c5ff47019384d914
Alejandro Moreno <alejowoods@gmail.com>
2024-01-17 17:53:29 +0100

    update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,26 +1,26 @@
-import React from "react";
-import "../App.css";
-import { NavLink } from "react-router-dom";
+import React from 'react'; 
+import '../App.css'; 
 
 const NavBar = () => {
-  return (
-    <nav>
-      <ul>
-        <li>
-          <NavLink path="/">HomePage</NavLink>
-        </li>
-        <li>
-          <img
-            src="Recipe Images\cook-book-logo-3-removebg-preview.png"
-            alt="cook-book"
-          />
-        </li>
-        <li>
-          <NavLink path="/recipes">Recipes</NavLink>
-        </li>
-      </ul>
-    </nav>
-  );
+    return (
+        <nav className='navbar'>
+            <a href="/">Home</a>
+            <a href="/about">About</a>
+            <a href="/contact">Contact</a>
+            <div className='dropdown'>
+                <a href="/recipes">Our Recipes</a>
+                <div className='dropdown-content'>
+                    <a href="/recipe1">Recipe 1</a>
+                    <a href="/recipe2">Recipe 2</a>
+                    <a href="/recipe3">Recipe 3</a>
+                    <a href="/recipe4">Recipe 4</a>
+                    <a href="/recipe5">Recipe 5</a>
+                    <a href="/recipe6">Recipe 6</a>
+                </div>
+            </div>
+        </nav>
+    )
 };
 
 export default NavBar;
+
