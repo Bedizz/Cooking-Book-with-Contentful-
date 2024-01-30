7ec2b380269bdc2bd0002f619be1be2bcc6d9d79
Bedizz <turkyilmaz.bediz@gmail.com>
2024-01-30 22:48:16 +0100

    some css

--- src/Pages/Recipe.jsx
+++ src/Pages/Recipe.jsx
@@ -19,56 +19,58 @@ const Recipe = ({ recipes }) => {
     navigate(-1)
   }
 
-
   return (
     <>
-      <div className={styles.container}>
-        <h2 className={styles.title}>{singleRecipe.fields.title}</h2>
-        <div className={styles["recipe-item"]}>
-          <img
-            className={styles["recipe-image"]}
-            src={singleRecipe.fields.image.fields.file.url}
-            alt={singleRecipe.fields.title}
-          />
-          <p className={styles.description}>
-            {singleRecipe.fields.description}
-          </p>
-        </div>
-      </div>
-
-      <div className="contentContainer">
-        <div
-          className={styles.toggle}
-          onClick={() => setShowIngredients(!showIngredients)}
-        >
-          Ingredients
-        </div>
-        {showIngredients && (
-          <div className={styles.ingredients}>
-            {singleRecipe.fields.ingredients.map((ingredient, index) => (
-              <p key={index}>{ingredient}</p>
-            ))}
+      {singleRecipe && (
+        <>
+          <div className={styles.container}>
+            <h2 className={styles.title}>{singleRecipe.fields.title}</h2>
+            <div className={styles["recipe-item"]}>
+              <img
+                className={styles["recipe-image"]}
+                src={singleRecipe.fields.image.fields.file.url}
+                alt={singleRecipe.fields.title}
+              />
+              <p className={styles.description}>
+                {singleRecipe.fields.description}
+              </p>
+            </div>
           </div>
-        )}
-        <div
-          className={styles.toggle}
-          onClick={() => setShowInstructions(!showInstructions)}
-        >
-          Procedure
-        </div>
-        {showInstructions && (
-          <div className={styles.instructions}>
-            {singleRecipe.fields.instructions}
+
+          <div className="contentContainer">
+            <div
+              className={styles.toggle}
+              onClick={() => setShowIngredients(!showIngredients)}
+            >
+              Ingredients
+            </div>
+            {showIngredients && (
+              <div className={styles.ingredients}>
+                {singleRecipe.fields.ingredients.map((ingredient, index) => (
+                  <p key={index}>{ingredient}</p>
+                ))}
+              </div>
+            )}
+            <div
+              className={styles.toggle}
+              onClick={() => setShowInstructions(!showInstructions)}
+            >
+              Procedure
+            </div>
+            {showInstructions && (
+              <div className={styles.instructions}>
+                {singleRecipe.fields.instructions}
+              </div>
+            )}
+
+            <div className="handleButtons">
+              <button onClick={handleClick}>Back to Home</button>
+              <button onClick={handleBack}>Back to Recipes</button>
+            </div>
           </div>
-        )}
-
-        <div className="handleButtons">
-          <button onClick={handleClick}>Back to Home</button>
-          <button onClick={handleBack}>Back to Recipes</button>
-        </div>
-      </div>
-
-      <Footer footerText={"With love from Turkey and Colombia"} />
+        </>
+      )}
+  <Footer footerText={"With love from Turkey and Colombia"} />
     </>
   );
 };
