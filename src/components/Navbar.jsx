e9b3d2f15bf5d35d4ac4f4b52f0a36132ca3d97b
Alejandro Moreno <alejowoods@gmail.com>
2024-02-04 14:55:49 +0100

    updating branch

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,32 +1,25 @@
-import React from "react";
-import "../App.css";
-import { NavLink, Link } from "react-router-dom";
-import styles from "./navbar.module.css";
-import BreadCrumbs from "./BreadCrumbs";
+import React from 'react'; 
+import '../App.css'; 
 
 const NavBar = () => {
-  return (
-    <>
-    <nav className={styles.navbar}>
-      <ul>
-        <li>
-          <NavLink to="/">HomePage</NavLink>
-        </li>
-        <li>
-          <img
-            src="Recipe Images\cook-book-logo-3-removebg-preview.png"
-            alt="cook-book"
-          />
-        </li> 
-        <li>
-          <NavLink to="/recipes">Recipes</NavLink>
-        </li>
-      </ul>
-    </nav>
-    <BreadCrumbs />
-    
-    </>
-  );
+    return (
+        <nav className='navbar'>
+            <a href="/">Home</a>
+            <a href="/about">About</a>
+            <a href="/contact">Contact</a>
+            <div className='dropdown'>
+                <a href="/recipes">Our Recipes</a>
+                <div className='dropdown-content'>
+                    <a href="/recipe1">Recipe 1</a>
+                    <a href="/recipe2">Recipe 2</a>
+                    <a href="/recipe3">Recipe 3</a>
+                    <a href="/recipe4">Recipe 4</a>
+                    <a href="/recipe5">Recipe 5</a>
+                    <a href="/recipe6">Recipe 6</a>
+                </div>
+            </div>
+        </nav>
+    )
 };
 
 export default NavBar;
